fa69e263e99426c93f75adf3e87d6b6767bb340f
Bruno Rech <brunno.rech@outlook.com>
2025-05-13 15:38:27 -0300

    feat: create custom error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
+import { HttpError } from "../utils/httpError";
 
 export const errorHandler = (
   err: any,
@@ -14,7 +15,14 @@ export const errorHandler = (
     });
   }
 
-  console.error("Unexpected error:", err);
+  if (err instanceof HttpError) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  console.log("err.constructor.name:", err.constructor?.name);
+  console.log(JSON.stringify(err));
+  console.log("err instanceof HttpError:", err instanceof HttpError);
+  //   console.error("Unexpected error:", err);
 
   return res.status(500).json({
     error: "Internal Server Error",
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,18 @@
 import { CreateUserDTO } from "../types/user";
 import { createUser } from "../repositories/users.repository";
+import { HttpError } from "../utils/httpError";
 
 export const createUserService = async (data: CreateUserDTO) => {
   if (!data.accepted_terms) {
-    throw new Error("Termos de uso não aceitos.");
+    throw new HttpError("Termos de uso não aceitos.", 400);
   }
 
   if (data.type === "INDIVIDUAL" && !data.cpf) {
-    throw new Error("CPF é obrigatório para pessoa física.");
+    throw new HttpError("CPF é obrigatório para pessoa física.", 400);
   }
 
   if (data.type === "COMPANY" && !data.cnpj) {
-    throw new Error("CNPJ é obrigatório para pessoa jurídica.");
+    throw new HttpError("CNPJ é obrigatório para pessoa jurídica.", 400);
   }
 
   return createUser(data);
