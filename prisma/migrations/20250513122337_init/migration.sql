7f575c5c227db535cd60463a3a3481f648efad6c
Bruno Rech <brunno.rech@outlook.com>
2025-05-13 09:26:33 -0300

    add env.example

---
+++ prisma/migrations/20250513122337_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accepted_terms_at` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `accepted_terms_at`;
